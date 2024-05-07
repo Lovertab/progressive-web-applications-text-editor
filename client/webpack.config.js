c18664a0c53f2f6b68074746356ba821c76035dc
Lovertab <lovertabrown@gmail.com>
2024-05-07 10:41:29 -0400

    update

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -12,51 +12,37 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-      // editor: './src/js/editor.js',
-      // header:'./src/js/header.js'
-    },
-    devServer: {
-      allowedHosts: 'all',
-      compress: true,
-      hot: true,
-      port: 3001,
-      // Other valid properties...
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      // Webpack plugin that generates our html file and injects our bundles. 
       new HtmlWebpackPlugin({
         template: './index.html',
-        title: 'Jate'
+        title: 'JATE  Text Editor',
       }),
-      // Creates a manifest.json file.
       new WebpackPwaManifest({
-        fingerprints: false,
-        inject: true,
-        name: 'Jate Info',
-        short_name: 'Jate',
-        description: 'Never forget your Jate!',
+        // inject: true,
+        name: 'JATE Text Editor',
+        short_name: 'JATE',
+        description: 'JATE in the browser.',
         background_color: '#225ca3',
         theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
-        display:'standalone',
+        publicPath: '/',
+        display: 'standalone',
         icons: [
           {
             src: path.resolve('src/images/logo.png'),
-            sizes: [96, 128, 144, 192, 256, 384, 512],
+            sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons'),
           },
           {
             src: path.resolve('favicon.ico'),
-            sizes: [96, 128, 144, 192, 256, 384, 512],
+            sizes: [96, 128, 192, 256, 384, 512],
           },
         ],
       }),
-      // Injects our custom service worker
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js',
@@ -64,7 +50,6 @@ module.exports = () => {
     ],
 
     module: {
-     // CSS loaders
       rules: [
         {
           test: /\.css$/i,
@@ -72,12 +57,11 @@ module.exports = () => {
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-          type:'asset/resource',
+          type: 'asset/resource',
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // We use babel-loader in order to use ES6.
           use: {
             loader: 'babel-loader',
             options: {
